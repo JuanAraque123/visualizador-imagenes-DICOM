ab36d99d58a61c94e43aac029b9265f1090cfc5e
JuanAraque123 <65414006+JuanAraque123@users.noreply.github.com>
2025-12-01 22:57:40 -0500

    Add files via upload

---
+++ dicom_viewer/__init__.py
@@ -0,0 +1,12 @@
+## File: dicom_viewer/__init__.py
+"""
+DICOM Viewer
+-----------
+Un visor de imágenes DICOM con capacidades de procesamiento de imágenes.
+"""
+
+__version__ = '1.0.0'
+
+"""
+Este archivo permite que el directorio dicom_viewer sea reconocido como un paquete Python.
+"""
